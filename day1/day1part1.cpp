b099e3bcfddc5ac172329cadb59338175127f85c
farisimba <farisimbaa@gmail.com>
2025-12-02 16:16:19 +0800

    day1part1 done, day1part2 in progress

---
+++ day1/day1part1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int rotateLeft (int point, int value) {
+    if (point - value < 0) {
+        return (point - value % 100 + 100) % 100;
+    } else return point - value;
+}
+
+int rotateRight (int point, int value) {
+    if (point + value >= 100) {
+        return (point + value % 100) % 100;
+    } else return point + value;
+}
+
+int rotate(int point, string rotation) {
+    if (rotation.front() == 'L') {
+        return rotateLeft(point, stoi(rotation.substr(1)));
+    } else {
+        return rotateRight(point, stoi(rotation.substr(1)));
+    }
+}
+
+int main() {
+    ifstream file("day1input.txt");
+    if (!file.is_open()) {
+        cerr << "Error: Cannot open day1input.txt" << endl;
+        return 1;
+    }
+
+    string rotation;
+    int point = 50;
+    int zeroCount = 0;
+
+    while (getline(file, rotation)) {
+        if (rotation == "") continue;
+        cout << "The dial is rotated " << rotation.front() << rotation.substr(1) << " to point at ";
+        point = rotate(point, rotation);
+        cout << point << endl;
+        if (point == 0) zeroCount++;
+    }
+
+    file.close();
+    cout << "The dial points at 0 a total of " << zeroCount << " times." << endl;
+    return zeroCount;
+}
