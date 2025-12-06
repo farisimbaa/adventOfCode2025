3911ea82e124d5e95c6c472cbfb91b57f8af26b9
farisimba <farisimbaa@gmail.com>
2025-12-07 01:16:03 +0800

    day5part1 done

---
+++ day5/day5part1.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+struct Range {
+    long long start;
+    long long end;
+};
+
+vector<Range> idRanges;
+
+bool isFresh(long long targetId) {
+    // cout << "Checking ID: " << targetId << endl;
+    for (Range range : idRanges) {
+        if (targetId >= range.start && targetId <= range.end) {
+            // cout << "ID found: " << id << endl;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    // ifstream file("day5example.txt");
+    ifstream file("day5input.txt");
+
+    if (!file.is_open()) {
+        return 1;
+    }
+    
+    int count = 0;
+    bool isRange = true;
+    string line;
+    idRanges.clear();
+
+    while (getline(file, line)) {
+        if (line.empty()) {
+            isRange = false;
+            continue;
+        }
+
+        if (isRange) {
+            size_t dashPos = line.find('-');
+            if (dashPos == string::npos) continue;
+
+            long long start = stoll(line.substr(0, dashPos));
+            long long end = stoll(line.substr(dashPos + 1));
+
+            idRanges.push_back({start, end});
+
+        } else {
+            long long id = stoll(line);
+            
+            if (isFresh(id)) {
+                count++;
+                cout << "Fresh ID: " << id << endl;
+            } else {
+                cout << "Spoiled ID: " << id << endl;
+            }
+        }
+    }
+
+    file.close();
+    
+    // for (int i : ids) {
+    //     cout << i << " ";
+    // }
+    
+    cout << "Total fresh IDs: " << count << endl;
+    return 0;
+}
