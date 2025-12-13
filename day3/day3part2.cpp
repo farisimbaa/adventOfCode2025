4d83cd31286ae54862bfa5de33c73ab79350c62b
farisimba <farisimbaa@gmail.com>
2025-12-13 20:17:00 +0800

    Update day3part2.cpp

--- day3/day3part2.cpp
+++ day3/day3part2.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
-int getProducedJoltage(string s) {
-    int len = s.length();
-    
+long long getProducedJoltage(string s) {
+    string joltage;
+    int pos = -1;
+    string target = "9";
+
+    pos = s.find(target);
+    if (s.substr(pos).length() >= 12) {
+        joltage += s[pos];
+    }
+
+    long long maxVal = stoll(joltage);
+    cout << "Produced joltage for " << s << " is " << maxVal << "." << endl;
+    return maxVal;
 }
 
 int main() {
-    int sum = 0;
-    // ifstream file("day3example.txt");
-    ifstream file("day3input.txt");
+    long long sum = 0;
+    ifstream file("day3example.txt");
+    // ifstream file("day3input.txt");
     if (!file.is_open()) {
         cerr << "Error: Cannot open file." << endl;
         return 1;
